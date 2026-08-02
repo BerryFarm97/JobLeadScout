293e315f1f19699ef440473af986f7d478b0187d
Austin Granbery <ab.granbery@gmail.com>
2026-08-02 18:46:23 -0500

    Connect job search form to refresh endpoint

--- app/static/dashboard.js
+++ app/static/dashboard.js
@@ -1,16 +1,37 @@
 const jobForm = document.getElementById("job-search-form");
 
-function refreshJobLeads(event) {
+
+async function refreshJobLeads(event) {
     event.preventDefault();
 
     const formData = new FormData(jobForm);
     const location = formData.get("location");
     const keywords = formData.get("keywords");
 
-    const locationParam = new URLSearchParams(location);
-    const keywordParam = new URLSearchParams(keywords);
+    const searchParams = new URLSearchParams();
 
-    console.log(locationParam.toString(), keywordParam.toString());
+    searchParams.set("keywords", keywords);
+    searchParams.set("location", location);
+
+    const queryString = searchParams.toString();
+    const refreshUrl = `/job-leads/refresh?${queryString}`;
+
+    try {
+        const response = await fetch(refreshUrl, {
+            method: "POST",
+        });
+
+        const jobResponse = await response.json();
+
+        if (response.ok) {
+            window.location.reload();
+        } else {
+            window.alert(jobResponse.detail);
+        }
+    } catch (error) {
+        console.error(error);
+        window.alert("Unable to complete request. Please try again later.");
+    }
 }
 
 jobForm.addEventListener("submit", refreshJobLeads);
